5472a0bca2a5d6a7a29f783f42409962785f4594
arinmis <mustafa_arinmis@outlook.com>
2022-07-15 19:53:05 +0300

    Return 204 when there is no length 5 word

--- wordle/api/views.py
+++ wordle/api/views.py
@@ -1,15 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.db.models.functions import Length
+from rest_framework import status
 from random import randrange
 from .models import Word
 
 @api_view(['GET'])
 def get_word(request):
     words = Word.objects.annotate(text_len=Length('word')).filter(text_len=5)
-    # return 204
+    # no word found 
     if (len(words) == 0):
-        pass
+        return Response(status=status.HTTP_204_NO_CONTENT)
     word = words[randrange(len(words))].word   
     return Response({"word": word.lower()})
 
