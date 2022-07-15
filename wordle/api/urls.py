eda28c1287ed3def6e69f339c700186b8b82acdf
arinmis <mustafa_arinmis@outlook.com>
2022-07-15 18:26:58 +0300

    Create word api endpoint

--- wordle/api/urls.py
+++ wordle/api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('word', views.get_word, name='get_word'),
 ]
--- wordle/api/views.py
+++ wordle/api/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+@api_view(['GET'])
+def get_word(request):
+    return Response({"word": "arinmis"})
 
 
