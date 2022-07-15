fa189201ceb311d71a995c6abbd1ef36834dad84
arinmis <mustafa_arinmis@outlook.com>
2022-07-15 19:47:36 +0300

    Return random number that has length 5

--- wordle/api/views.py
+++ wordle/api/views.py
@@ -1,8 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.db.models.functions import Length
+from random import randrange
+from .models import Word
 
 @api_view(['GET'])
 def get_word(request):
-    return Response({"word": "arinmis"})
+    words = Word.objects.annotate(text_len=Length('word')).filter(text_len=5)
+    # return 204
+    if (len(words) == 0):
+        pass
+    word = words[randrange(len(words))].word   
+    return Response({"word": word.lower()})
 
 
