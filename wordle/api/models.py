2bf4ac928be4aa31fe0b4d7f4c02eaf62c76fc5f
arinmis <mustafa_arinmis@outlook.com>
2022-07-17 14:28:05 +0300

    Add english words to model and DB

--- wordle/api/models.py
+++ wordle/api/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Word(models.Model):
+
+    class Language(models.TextChoices):
+        EN = 'tr', _('Turkish')
+        TR = 'en', _('English')
+
+    lang = models.CharField(max_length=2, choices=Language.choices)
     word = models.CharField(max_length=200)
