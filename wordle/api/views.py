17bf5669049e929418099c919bac4ab4f513bd92
arinmis <mustafa_arinmis@outlook.com>
2022-07-17 14:46:02 +0300

    Check is requested language valid

--- wordle/api/views.py
+++ wordle/api/views.py
@@ -7,6 +7,11 @@ from .models import Word
 
 @api_view(['GET'])
 def get_word(request, lang, length, amount) :
+    # check language
+    supported_langs = [lang[0] for lang in Word.Language.choices]
+    if (not (lang in supported_langs)):
+        return Response("Supported languages: {}" .format(",".join(supported_langs)), status=status.HTTP_400_BAD_REQUEST)
+
     words = Word.objects.annotate(text_len=Length('word')).filter(text_len=length, lang=lang)
     # no word found 
     if (len(words) == 0 or amount > len(words) ):
